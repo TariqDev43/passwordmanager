029ad1e0d1a2ff5d6745eade67b6763558d56f4e
krowndev <tariqtahir43@yahoo.com>
2023-02-17 17:41:14 +0500

    added animation in baseNav

--- tariq/components/BaseNavigation.js
+++ tariq/components/BaseNavigation.js
@@ -1,77 +1,80 @@
-import { View, TouchableOpacity, StyleSheet } from "react-native";
-import React, { memo, useState } from "react";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import tw from "tailwind-react-native-classnames";
+import { View, TouchableOpacity, StyleSheet } from 'react-native';
+import React, { memo, useState } from 'react';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import tw from 'tailwind-react-native-classnames';
 
-import { useNavigation } from "@react-navigation/native";
-import useTheme from "../Contexts/ThemeContext";
+import { useNavigation } from '@react-navigation/native';
+import useTheme from '../Contexts/ThemeContext';
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
 
 const BaseNavigation = () => {
+  /*   ALL STATES
+   ********************************************* */
+  //  all states
   const { theme } = useTheme();
   const navigation = useNavigation();
-  const [selected, setSelected] = useState("home");
+
+  const [selected, setSelected] = useState('home');
+
+  // All animation
+  const offset = useSharedValue(15);
+
+  const animatedNavStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: withTiming(offset.value, { duration: 200 }) }],
+    };
+  });
+  /*   ALL FUNCTIONS
+   ********************************************* */
+
   const nav = [
     {
-      icon: "home",
-      path: "Home",
+      icon: 'home',
+      path: 'Home',
     },
     {
-      icon: "heart",
-      path: "Fav",
+      icon: 'heart',
+      path: 'Fav',
     },
     {
-      icon: "cog",
-      path: "Settings",
+      icon: 'cog',
+      path: 'Settings',
     },
   ];
   return (
     <View
       style={[
-        tw`absolute self-center bottom-5 rounded-2xl 
-      flex-row py-1 w-4/6 px-1`,
-        { ...styles.navShadow, backgroundColor: theme.mainColor },
+        tw`absolute justify-evenly self-center bottom-5 rounded-2xl 
+      flex-row py-1 px-1`,
+        { backgroundColor: theme.mainColor, width: 240 },
       ]}
     >
+      <Animated.View
+        style={[
+          tw`bg-white px-7 left-0 py-5 rounded-xl self-center absolute `,
+          { transform: [{ translateX: offset.value }] },
+          animatedNavStyle,
+        ]}
+      ></Animated.View>
       {nav.map((i) => (
         <TouchableOpacity
-          onPressIn={() => {
+          onPress={() => {
             navigation.navigate(i.path);
             setSelected(i.icon);
+            offset.value = i.icon == 'home' ? 15 : i.icon == 'heart' ? 93 : 168;
           }}
           key={i.icon}
-          style={tw`justify-center py-1 items-center flex-1`}
+          style={[tw`my-2 flex-1 justify-center items-center`, {}]}
         >
-          <View style={[tw`w-3/4 px-2`]}>
-            <View
-              style={[
-                tw`py-2 justify-center items-center rounded-full`,
-                {
-                  backgroundColor:
-                    i.icon == selected ? "white" : theme.mainColor,
-                },
-              ]}
-            >
-              <MaterialCommunityIcons
-                name={i.icon == selected ? i.icon : `${i.icon}-outline`}
-                size={30}
-                color={i.icon == selected ? theme.mainColor : "white"}
-              />
-            </View>
-          </View>
+          <MaterialCommunityIcons
+            name={i.icon == selected ? i.icon : `${i.icon}-outline`}
+            size={30}
+            color={i.icon == selected ? theme.mainColor : 'white'}
+          />
         </TouchableOpacity>
       ))}
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  navShadow: {
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.5,
-    shadowRadius: 2,
-    elevation: 2,
-  },
-});
 
 export default memo(BaseNavigation);
